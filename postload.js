72f5299309c3d412c409f8feb9407605dc7032d3
lauragrauer <lg845@drexel.edu>
2025-03-20 22:49:30 -0400

    f
    f

--- postload.js
+++ postload.js
@@ -88,7 +88,7 @@ function displayZodInfo(whichOne) {
             zDescriptionObj.innerHTML = 'The Harpy Eagle…a proud and stealthy hunter, well regarded as one of the more independent creatures in the birds of prey category. With a commanding presence, the Harley Eagle reflects a self sufficient personality with those born in this time period.  Just as the apex predator focuses on its prey with precision, these guys are equally driven to achieve their goals. Natural leaders with a clear direction can often become rather solitary in their pursuit of success, but are heavily committed to their values, ensuring they stand out as an accomplishment.';
             DisplayAreaObj.classList.remove("hideMe");
             // PLAY AUDIO FOR THIS CHARACTER
-            playAudio('bloop');
+            playAudio('HARPYEAGLE');
             break;
         case 'MuteSwan':
             zImageObj.src = 'images/mute-swan.gif';
@@ -96,7 +96,7 @@ function displayZodInfo(whichOne) {
             zDescriptionObj.innerHTML = 'Known for love! The swan embodies the romantic and thoughtful nature of February. Similar to those born in this month, swans possess a deep sense of loyalty in an elegant manner, often seen as symbols of love and partnership. However, similar to the months climate itself, February can bring a mix of cold and unpredictable moments, bringing out the swans more guarded, sensitive side; this can cause them to become a bit assertive and overly sentimental. Despite these occasional challenges, the swans ability to love deeply and remain devoted to their partner makes them a beautiful representation of a heartfelt connection.';
             DisplayAreaObj.classList.remove("hideMe");
             // PLAY AUDIO FOR THIS CHARACTER
-            playAudio('crack');
+            playAudio('MUTESWAN');
             break;
         case 'BaltimoreOriole':
             zImageObj.src = 'images/baltimoreorielle.gif';
@@ -104,7 +104,7 @@ function displayZodInfo(whichOne) {
             zDescriptionObj.innerHTML = 'Natural artists at heart, brimming with creativity and imagination! With its vibrant plumage, they bring a unique and refreshing energy to everything they do, leaving a lasting impression wherever they go. They offer a sense of renewal, much like their migratory arrival in the spring. Though they thrive in a lively setting, they also value quiet moments of introspection. Sometimes a small break of solitude to recharge aligns with the refinement of their vision. While they may appear reserved at times, their potential for artistic expression and personal growth knows no bounds.';
             DisplayAreaObj.classList.remove("hideMe");
             // PLAY AUDIO FOR THIS CHARACTER
-            playAudio('crack');
+            playAudio('BALTIMOREORIOLE');
             break;
         case 'StellersJay':
             zImageObj.src = 'images/stellarjay.gif';
@@ -112,7 +112,7 @@ function displayZodInfo(whichOne) {
             zDescriptionObj.innerHTML = 'With its unique blue plumage and bold crest, the Stellers Jay stands out in the western regions due to its distinctive appearance. Much like those born in April, it embodies a sense of curiosity and enthusiasm, constantly seeking out new experiences. However, dont be fooled by this vibrant exterior for long. The Stellers Jay is known for its impulsive and mischievous nature, often diving into adventures that dont always go as planned. Despite these moments of unpredictability, these birds are highly loyal and form long lasting bonds with a small group of companions, showing that beneath the playful demeanor lies a dependable commitment to those they care about.';
             DisplayAreaObj.classList.remove("hideMe");
             // PLAY AUDIO FOR THIS CHARACTER
-            playAudio('crack');
+            playAudio('STELLERSJAY');
             break;
         case 'RedCrestedCardinal':
             zImageObj.src = 'images/redcreastedcardinal.gif';
@@ -120,7 +120,7 @@ function displayZodInfo(whichOne) {
             zDescriptionObj.innerHTML = 'Be careful… youll pull others like a magnet! The Red Crested Cardinals are often a charismatic bunch, full with energy to go alongside. The lively red colored crest mirrors the cardinals vibrant personality, often attracting attention in social situations. Known for their striking appearance, these aviaries also carry a sharp mindset and zest for life, easily able to captivate others with their talent and enthusiasm. While they may struggle with the occasional indecision, their confident song and presence exemplifies the Red Crested Cardinals knack for adapting to seasonal changes and inspiring thought around them with their innate charm.';
             DisplayAreaObj.classList.remove("hideMe");
             // PLAY AUDIO FOR THIS CHARACTER
-            playAudio('crack');
+            playAudio('REDCRESTEDCARDINAL');
             break;
         case 'Roadrunner':
             zImageObj.src = 'images/roadrunner.gif';
@@ -128,7 +128,7 @@ function displayZodInfo(whichOne) {
             zDescriptionObj.innerHTML = 'People born in June often possess a unique combination of sensitivity and resourcefulness, much like the Road Runner. This bird is known for its quickness, cleverness, and ability to adapt in difficult situations. Like the Road Runner, June-borns are perceptive individuals who can read the emotions of others and know when to act swiftly, but they often prefer to take time to recharge in solitude. Their sensitive and introspective sides make them empathetic, but they also have an energetic and adventurous spirit that drives them to seek new experiences and challenges. Despite their emotional depth, they sometimes avoid confrontation and prefer to keep things lighthearted, much like the playful nature of the Road Runner.';
             DisplayAreaObj.classList.remove("hideMe");
             // PLAY AUDIO FOR THIS CHARACTER
-            playAudio('crack');
+            playAudio('ROADRUNNER');
             break;
         case 'Seagull':
             zImageObj.src = 'images/gull.gif';
@@ -136,7 +136,7 @@ function displayZodInfo(whichOne) {
             zDescriptionObj.innerHTML = 'July-born individuals are typically adventurous and independent, with a strong desire for personal freedom, much like the Seagull. These birds are often seen roaming coastal areas, highly self-sufficient, and not afraid to assert their territory. Similarly, July-borns are often self-reliant, yet they can be moody and temperamental at times… Especially when they feel their autonomy is threatened. Much like the gull, they have a sharp wit and a sense of resilience, always able to weather challenges and keep moving forward. However, like the gulls loud, sharp calls, July-borns can sometimes express their emotions with intensity, especially when confronted with adversity.';
             DisplayAreaObj.classList.remove("hideMe");
             // PLAY AUDIO FOR THIS CHARACTER
-            playAudio('crack');
+            playAudio('SEAGULL');
             break;
         case 'AcornWoodpecker':
             zImageObj.src = 'images/acornwoodpecker.gif';
@@ -144,7 +144,7 @@ function displayZodInfo(whichOne) {
             zDescriptionObj.innerHTML = 'The Acorn Woodpecker is a true embodiment of generosity and community, traits that reflect the essence of those born in August. August-borns are known for their hardworking and philanthropic nature, just as the Acorn Woodpecker works tirelessly to store acorns and support its community. They are very self-motivated, always striving for the betterment of their loved ones, and have an innate sense of loyalty. Like the woodpecker, August-born individuals are grounded and dependable, with a strong focus on their family and close-knit relationships. They may appear serious or intense in their efforts, but their commitment to others makes them incredibly compassionate and giving.';
             DisplayAreaObj.classList.remove("hideMe");
             // PLAY AUDIO FOR THIS CHARACTER
-            playAudio('crack');
+            playAudio('ACORNWOODPECKER');
             break;
         case 'GreatBlueHeron':
             zImageObj.src = 'images/greatbluehearon.gif';
@@ -152,7 +152,7 @@ function displayZodInfo(whichOne) {
             zDescriptionObj.innerHTML = 'People born in September are often intelligent, spiritual, and reflective, much like the Great Blue Heron. Known for its composed and patient demeanor, the heron mirrors the calm, introspective nature of September-borns who often seek clarity and wisdom in their pursuits. Similar to the herons deliberate hunting style, these individuals tend to take their time in making decisions, weighing their options carefully before acting. The herons solitary nature also aligns with September-borns introspective tendencies, as they often prefer quiet spaces for deep reflection and contemplation, making them both thoughtful and empathetic.';
             DisplayAreaObj.classList.remove("hideMe");
             // PLAY AUDIO FOR THIS CHARACTER
-            playAudio('crack');
+            playAudio('GREATBLUEHERON');
             break;
         case 'CanadaGoose':
             zImageObj.src = 'images/goose.gif';
@@ -160,7 +160,7 @@ function displayZodInfo(whichOne) {
             zDescriptionObj.innerHTML = 'The Canada Goose is an ideal representation of October-born individuals, as it symbolizes loyalty and determination. These individuals are known for their resilience and strength, as well as their ability to build strong connections with those they deeply care for. Their migratory nature also reflects the October-borns ability to adapt to new circumstances and overcome challenges. Much like the birds constant need for companionship during migration, October-borns thrive in close communities, relying on their strong sense of loyalty and authenticity to guide them through challenges. They value their relationships and make powerful bonds that last through time, similar to a Canada Gooses enduring partnerships within flocks.';
             DisplayAreaObj.classList.remove("hideMe");
             // PLAY AUDIO FOR THIS CHARACTER
-            playAudio('crack');
+            playAudio('CANADAGOOSE');
             break;
         case 'CarolinaChickadee':
             zImageObj.src = 'images/carolinachickadee.gif';
@@ -168,7 +168,7 @@ function displayZodInfo(whichOne) {
             zDescriptionObj.innerHTML = 'The Carolina Chickadee embodies the personality of those born in November, with its sharp, perceptive nature and empathy. Known for its curiosity and generosity, the chickadee is a social bird that thrives on strong relationships within its community, much like the November-born who excel at motivating and supporting their loved ones. These individuals tend to be very perceptive, noticing details that others may miss, and have a natural ability to inspire and encourage those around them. Like the chickadees ability to adapt to changes in its environment, November-borns are resilient and driven. Regardless, they also prefer to avoid unnecessary praise and spotlight, much like the humble and understated nature of the Carolina Chickadee.';
             DisplayAreaObj.classList.remove("hideMe");
             // PLAY AUDIO FOR THIS CHARACTER
-            playAudio('crack');
+            playAudio('CAROLINACHICKADEE');
             break;
         case 'SnowyOwl':
             zImageObj.src = 'images/snowowl.gif';
@@ -176,7 +176,7 @@ function displayZodInfo(whichOne) {
             zDescriptionObj.innerHTML = 'The Snowy Owl symbolizes wisdom, independence, and clarity of thought. Much like the owl, December-born individuals possess a quiet strength and are often patient when it comes to making decisions. They are very independent, with a deep sense of personal responsibility. The Snowy Owl, often associated with winters stark beauty, reflects the December-borns preference for solitude, where they can focus on their goals without distraction. However, just as the owl’s keen vision allows it to see clearly in the dark, December-borns are able to navigate through life with a unique insight, often making decisions that others may not immediately understand but that prove to be grounded and wise in the long run.';
             DisplayAreaObj.classList.remove("hideMe");
             // PLAY AUDIO FOR THIS CHARACTER
-            playAudio('crack');
+            playAudio('SNOWYOWL');
             break;
         case 'Question':
             zImageObj.src = 'images/questionmark1.png';
@@ -184,7 +184,7 @@ function displayZodInfo(whichOne) {
             zDescriptionObj.innerHTML = 'Enter your birthday to discover the bird that best represents your special day! Each bird is carefully chosen to align with the traits and symbolism associated with the month you were born. Lets find out which bird connects with you!';
             DisplayAreaObj.classList.remove("hideMe");
             // PLAY AUDIO FOR THIS CHARACTER
-            playAudio('crack');
+            playAudio('chimes');
             break;
         default:
             console.log('ERROR WRONG ONE!');
